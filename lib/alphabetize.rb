313ab68eedf3e149fe355f7d0c8e6dd211eeaf59
Tony J. Lum <Tonyjlum@gmail.com>
2018-10-31 21:12:21 -0400

    diff lang

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,24 @@
+require 'pry'
+
+ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
-  # code here
+  alpha_hsh = {}
+  ALPHABET.chars.each.with_index { |char, idx| alpha_hsh[char] = idx }
+
+  arr.each.with_index do |word,idx1|
+    arr.each.with_index do |word, idx2|
+      next if idx1 == idx2
+
+      i = 0
+      if alpha_hsh[arr[idx1][i]] == alpha_hsh[arr[idx2][i]]
+        i+=1 while alpha_hsh[arr[idx1][i]] == alpha_hsh[arr[idx2][i]]
+      end
+
+      if alpha_hsh[arr[idx1][i]] < alpha_hsh[arr[idx2][i]]
+        arr[idx1], arr[idx2] = arr[idx2], arr[idx1]
+      end
+
+    end
+  end
+  arr
 end
