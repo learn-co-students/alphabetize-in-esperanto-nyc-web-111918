5b58f6f8030905d63958409f4cfcdc70044deec4
Damon Collins <damon.c.p.collins@gmail.com>
2018-11-09 09:55:19 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
 def alphabetize(arr)
   # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  alphabet = esperanto_alphabet.chars 
+  arr.sort_by do |word| 
+    word.chars.map do |letter|
+      alphabet.index(letter)
+    end
+  end 
 end
