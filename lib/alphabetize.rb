c18b0d067ca3b0836eee86bea998d661bce0c3a8
satyasinha94 <satyasinha94@gmail.com>
2018-11-14 17:51:06 -0500

    Done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,7 @@
-def alphabetize(arr)
-  # code here
+require 'pry'
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+def alphabetize(expressions)
+  expressions.sort_by{|expression| expression.chars.map{|char| ESPERANTO_ALPHABET.index(char)}}
 end
